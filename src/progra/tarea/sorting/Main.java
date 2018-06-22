d26ca30e38993e994621973b019d056d07c23e45
Your Name git config --global user.name Your <javier06herrera@gmail.com>
2018-06-21 22:12:55 -0600

    Fail no guarde

--- src/progra/tarea/sorting/Main.java
+++ src/progra/tarea/sorting/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Testeadores tester=new Testeadores();
-		double promedioS=0;
+		double promedioS=tester.Selection();
 		double promedioQ=tester.QuickSort();
 		
 		System.out.println("Selection: "+promedioS+" Quick: "+promedioQ);
